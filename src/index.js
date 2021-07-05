c3e32efe88eda8116eeac21458e76f8d724f37f2
Oleg Baranov <oleg.baranov@skywindgroup.com>
2021-07-05 18:26:04 +0300

    feat: lesson 98: useContext()

--- src/index.js
+++ src/index.js
@@ -1,25 +1,34 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import ReactDOM from 'react-dom';
+import { TestConext } from './context';
 
 
 const HookSwitcher = () => {
 
+  const context = useContext(TestConext);
   const [color, setColor] = useState('white');
   const [fontSize, setFontSize] = useState(14);
 
   return (
     <div style={{ padding: '10px', backgroundColor: color, fontSize: fontSize + 'px' }}>
-      Some Text
       <button onClick={() => setColor('red')}>Red</button>
       <button onClick={() => setColor('white')}>White</button>
       <button onClick={() => setFontSize(s => s + 1)}>+</button>
+      <br/>
+      Context value: {context.value}
+      <button onClick={() => context.change(context.value + 1)}>{context.label}</button>
     </div>
   )
 }
 
 const App = () => {
+
+  const [ contextValue, setContextValue ] = useState(10);
+
   return <div>
-    <HookSwitcher/>
+    <TestConext.Provider value={{label: 'Context Button', value: contextValue, change: setContextValue}}>
+      <HookSwitcher />
+    </TestConext.Provider>
   </div>
 }
 
