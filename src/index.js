88a17abfefe188302c3fdfc046d227a3cfee1037
Oleg Baranov <oleg.baranov@skywindgroup.com>
2021-07-05 18:06:13 +0300

    feat: lesson 97: useState()

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+
+const HookSwitcher = () => {
+
+  const [color, setColor] = useState('white');
+  const [fontSize, setFontSize] = useState(14);
+
+  return (
+    <div style={{ padding: '10px', backgroundColor: color, fontSize: fontSize + 'px' }}>
+      Some Text
+      <button onClick={() => setColor('red')}>Red</button>
+      <button onClick={() => setColor('white')}>White</button>
+      <button onClick={() => setFontSize(s => s + 1)}>+</button>
+    </div>
+  )
+}
+
 const App = () => {
-  return <h1>Hi!</h1>
+  return <div>
+    <HookSwitcher/>
+  </div>
 }
 
 ReactDOM.render(
